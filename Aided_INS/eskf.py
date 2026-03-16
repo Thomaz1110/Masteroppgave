54d344c142b68d232071484d9c6190f3fa28f621
Thomaz1110 <thomashd4u@gmail.com>
2026-03-16 13:19:58 +0100

    Fixed discretizing of Q in Aided_INS

--- Aided_INS/eskf.py
+++ Aided_INS/eskf.py
@@ -33,36 +33,37 @@ class ESKFMultiRobot:
 
 
         q_acc = sigma_acc ** 2
-        q_b = sigma_bias ** 2
+        q_b = (sigma_bias ** 2) / self.dt
 
         self.Ac = np.zeros((self.state_dim, self.state_dim))
         self.Ec = np.zeros((self.state_dim, self.noise_dim))
 
         for robot in range(num_robots):
-            initiator_offset = robot * 6
+            state_offset = robot * 6
             noise_offset = robot * 4
             
             # Continuous-time system matrix
-            self.Ac[initiator_offset + 0, initiator_offset + 2] = 1.0
-            self.Ac[initiator_offset + 1, initiator_offset + 3] = 1.0
-            self.Ac[initiator_offset + 2, initiator_offset + 4] = -1.0
-            self.Ac[initiator_offset + 3, initiator_offset + 5] = -1.0
-            self.Ac[initiator_offset + 4, initiator_offset + 4] = decay
-            self.Ac[initiator_offset + 5, initiator_offset + 5] = decay
+            self.Ac[state_offset + 0, state_offset + 2] = 1.0
+            self.Ac[state_offset + 1, state_offset + 3] = 1.0
+            self.Ac[state_offset + 2, state_offset + 4] = -1.0
+            self.Ac[state_offset + 3, state_offset + 5] = -1.0
+            self.Ac[state_offset + 4, state_offset + 4] = decay
+            self.Ac[state_offset + 5, state_offset + 5] = decay
 
             # Continuous-time process noise matrix
-            self.Ec[initiator_offset + 2, noise_offset + 0] = -1.0
-            self.Ec[initiator_offset + 3, noise_offset + 1] = -1.0
-            self.Ec[initiator_offset + 4, noise_offset + 2] = 1.0
-            self.Ec[initiator_offset + 5, noise_offset + 3] = 1.0
+            self.Ec[state_offset + 2, noise_offset + 0] = -1.0
+            self.Ec[state_offset + 3, noise_offset + 1] = -1.0
+            self.Ec[state_offset + 4, noise_offset + 2] = 1.0
+            self.Ec[state_offset + 5, noise_offset + 3] = 1.0
 
         # Discrete-time system matrix and process noise matrix
         self.Ad = np.eye(self.state_dim) + self.Ac * self.dt
         self.Ed = self.Ec.copy() * self.dt
 
         # Discrete-time process noise covariance
-        Q_block = np.diag([q_acc, q_acc, q_b, q_b])
-        self.Qd = np.kron(np.eye(num_robots), Q_block)
+        Qc_block = np.diag([q_acc, q_acc, q_b, q_b])
+        self.Qc = np.kron(np.eye(num_robots), Qc_block)
+        self.Qd = self.Ed @ self.Qc @ self.Ed.T
 
         self.deltax = np.zeros((self.state_dim, 1))
         self.P = np.eye(self.state_dim) * 1e-3
@@ -72,27 +73,27 @@ class ESKFMultiRobot:
  
     def predict(self):
         self.deltax = self.Ad @ self.deltax # not really necessary since deltax is reset after each update, but included for completeness
-        self.P = self.Ad @ self.P @ self.Ad.T + self.Ed @ self.Qd @ self.Ed.T 
+        self.P = self.Ad @ self.P @ self.Ad.T + self.Qd 
 
     def update(self, robot_index, update_type, y_meas, y_ins_hat, initiator_nom=None, reflector_nom=None, reflector_index=None):
-        initiator_offset = robot_index * 6                            # 6 states per robot
+        state_offset = robot_index * 6                            # 6 states per robot
 
         if update_type == "velocity_x":
             C = np.zeros((1, self.state_dim))
-            C[0, initiator_offset + 2] = 1.0
+            C[0, state_offset + 2] = 1.0
             R = np.array([[self.sigma_vel**2]])
             delta_y = (y_meas - y_ins_hat).reshape(1, 1)
 
         elif update_type == "velocity_y":
             C = np.zeros((1, self.state_dim))
-            C[0, initiator_offset + 3] = 1.0
+            C[0, state_offset + 3] = 1.0
             R = np.array([[self.sigma_vel**2]])
             delta_y = (y_meas - y_ins_hat).reshape(1, 1)
 
         elif update_type == "velocity_calibration":
             C = np.zeros((2, self.state_dim))
-            C[0, initiator_offset + 2] = 1.0
-            C[1, initiator_offset + 3] = 1.0
+            C[0, state_offset + 2] = 1.0
+            C[1, state_offset + 3] = 1.0
             R = np.diag([self.sigma_vel**2, self.sigma_vel**2])
             delta_y = (y_meas - y_ins_hat).reshape(2, 1)
 
@@ -107,8 +108,8 @@ class ESKFMultiRobot:
             C = np.zeros((1, self.state_dim))
             diff = initiator_nom - reflector_nom
             dist = max(np.linalg.norm(diff), 1e-9)
-            C[0, initiator_offset + 0] = diff[0] / dist
-            C[0, initiator_offset + 1] = diff[1] / dist
+            C[0, state_offset + 0] = diff[0] / dist
+            C[0, state_offset + 1] = diff[1] / dist
 
             if update_type == "robot_range":
                 reflector_offset = reflector_index * 6
